9bcd91dcf89075a52f7e5d04729b8af65810a7b0
gopala <gopal1852000@gmail.com>
2025-07-21 14:18:41 +0530

    damn a lot of pain with mono

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+
+import {client} from '@repo/db/client'
+
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send("hi there")
+});
+
+
+app.post('/signup',async (req, res)=>{
+
+    const { email, password} = req.body;
+    try {
+        const response  = await client.user.create({
+            data: {
+                email,
+                password
+            }
+            
+        });
+
+        res.json({
+            msg: "user created"
+        })
+
+
+    } catch (error) {
+        res.json({
+            msg: "database error"
+        })
+    }
+});
+
+app.listen(3000);
